2d684ad1a609438a5121a849b0fd498b82fae33e
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2026-02-07 19:14:14 -0800

    Add mise task for formatting JS/CSS + formatting changes

--- vancouver_sunrise/init.js
+++ vancouver_sunrise/init.js
@@ -7,19 +7,27 @@
         if (currentDate !== lastDate) {
             console.log("Vancouver Sunrise: Date changed, updating toolbar...");
 
-            xhr.post("backend.php", App.getPostReply({ op: "pluginhandler", plugin: "vancouver_sunrise", method: "update" }), (reply) => {
-                const container = document.querySelector(".vancouver-sunrise-container");
-                if (container && reply.responseText) {
-                    // Update the inner content or replace the container
-                    const temp = document.createElement("div");
-                    temp.innerHTML = reply.responseText;
-                    const newContainer = temp.firstChild;
-                    if (newContainer) {
-                        container.parentNode.replaceChild(newContainer, container);
-                        lastDate = currentDate;
+            xhr.post(
+                "backend.php",
+                App.getPostReply({
+                    op: "pluginhandler",
+                    plugin: "vancouver_sunrise",
+                    method: "update",
+                }),
+                (reply) => {
+                    const container = document.querySelector(".vancouver-sunrise-container");
+                    if (container && reply.responseText) {
+                        // Update the inner content or replace the container
+                        const temp = document.createElement("div");
+                        temp.innerHTML = reply.responseText;
+                        const newContainer = temp.firstChild;
+                        if (newContainer) {
+                            container.parentNode.replaceChild(newContainer, container);
+                            lastDate = currentDate;
+                        }
                     }
                 }
-            });
+            );
         }
     };
 
